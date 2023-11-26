da9300a696aad30fa8ca25237a60239065585c30
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2023-11-26 10:03:55 +0100

    Docs: Add index, restructure conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,40 +1,11 @@
-# Minimal conf.py for Sphinx documentation
-
 # -- Project information -----------------------------------------------------
-
 project = "World model prototype"
 author = "Ewout ter Hoeven"
 
 # -- General configuration ---------------------------------------------------
+extensions = ['recommonmark']  # Add recommonmark for Markdown support
 
-extensions = []
-
-# -- Add policy, behaviors and docs/source to path --------------------------------------
+# -- Path setup --------------------------------------------------------------
 import os
 import sys
-import glob
-
-sys.path.insert(0, os.path.abspath("../.."))
-
-source_dirs = ["policy", "docs/source"]
-
-# Use glob to find all README.md files in src/behaviors and its subdirectories
-behaviors_readmes = glob.glob(
-    os.path.join(os.path.dirname(__file__), "../../src/behaviors/**/README.md"),
-    recursive=True,
-)
-policy_docs = glob.glob(
-    os.path.join(os.path.dirname(__file__), "../../policy/**/*.md"), recursive=True
-)
-
-# Define the toctree with the dynamically generated list of README.md files
-toctree_behaviors = [
-    "src/behaviors/" + os.path.relpath(readme, os.path.dirname(__file__))
-    for readme in behaviors_readmes
-]
-toctree_policy = [
-    "policy/" + os.path.relpath(doc, os.path.dirname(__file__)) for doc in policy_docs
-]
-
-# Append the dynamically generated list of README.md files to the TOCTREE
-source_dirs.extend(toctree_behaviors)
+sys.path.insert(0, os.path.abspath('../..'))
---
+++ docs/source/index.rst
@@ -0,0 +1,13 @@
+.. World model prototype documentation master file
+
+World Model Prototype Documentation
+===================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   behavior_docs
+   policy_docs
