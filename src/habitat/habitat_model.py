768f63fd1ea44aa9d02d875c736fdc9286fb9516
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2023-12-11 14:02:49 +0100

    habitat: Implement some agent states

--- src/habitat/habitat_model.py
+++ src/habitat/habitat_model.py
@@ -1,14 +1,14 @@
 from mesa import Model, Agent
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
-
+import random
 
 class HabitatModel(Model):
     def __init__(self, n_prey, n_predators):
         self.schedule = None
         self.running = True
         self.schedule = RandomActivation(self)
-        self.grid = MultiGrid
+        self.grid = MultiGrid(32, 32, True)
 
         # Create prey agents
         for i in range(n_prey):
@@ -33,7 +33,11 @@ class HabitatModel(Model):
 class Animal(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-
+        self.sleep: float = random.randomfloat(0.25, 0.75)
+        self.energy: float = random.randomfloat(0.25, 0.75)
+        self.health: float = random.randomfloat(0.5, 1.0)
+        self.age: int = random.randint(0, 10)
+        self.lust: float = random.randomfloat(0.25, 0.75) if self.age > 2 else 0.0
 
 class Prey(Animal):
     def __init__(self, unique_id, model):
@@ -41,7 +45,6 @@ class Prey(Animal):
 
     def step(self):
         pass
-
 
 class Predator(Animal):
     def __init__(self, unique_id, model):
